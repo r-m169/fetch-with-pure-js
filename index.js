df7aa57fcabc7f86d9f7cd26c672ad4aaf99e084
rodaynamohamed <rodaynamohamed169@gmail.com>
2023-10-14 22:20:59 +0300

    adding style

--- index.js
+++ index.js
@@ -2,60 +2,68 @@ fetch('https://dummyjson.com/products')
     .then(response => response.json())
     .then(data => {
         const productsList = data.products;
-        console.log(productsList)
-
         const productsContainer = document.getElementById('products-container');
 
-    // ...
-productsList.forEach(product => {
-    const card = document.createElement('div');
-    card.classList.add('card'); 
+        productsList.forEach(product => {
+            const card = document.createElement('div');
+            card.classList.add('card');
 
-    const cardImage = document.createElement('img');
-    cardImage.classList.add('card-img-top');
-    cardImage.src = product.thumbnail; 
+            const cardImage = document.createElement('img');
+            cardImage.classList.add('card-img-top');
+            cardImage.src = product.thumbnail;
 
-    const cardBody = document.createElement('div');
-    cardBody.classList.add('card-body');
+            const cardBody = document.createElement('div');
+            cardBody.classList.add('card-body');
 
-    const cardTitle = document.createElement('h5');
-    cardTitle.classList.add('card-title');
-    cardTitle.textContent = product.title;
+            const cardTitle = document.createElement('h5');
+            cardTitle.classList.add('card-title');
+            cardTitle.textContent = product.title;
 
-    const cardText = document.createElement('p');
-    cardText.classList.add('card-text');
-    cardText.textContent = product.description;
+            const cardText = document.createElement('p');
+            cardText.classList.add('card-text');
+            cardText.textContent = product.description;
 
-    // Calculate the discounted price
-    const price = product.price;
-    const discountPercentage = product.discountPercentage;
-    const discountedPrice = (price * discountPercentage) / 100;
-    const finalPrice = price - discountedPrice; 
+            // Calculate the discounted price
+            const price = product.price;
+            const discountPercentage = product.discountPercentage;
+            const discountedPrice = (price * discountPercentage) / 100;
+            const finalPrice = price - discountedPrice;
 
-    const priceElement = document.createElement('p');
-    priceElement.textContent = ` ${finalPrice.toFixed(2)} / ${price.toFixed(2)}`;
+            const priceContainer = document.createElement('div');
+            priceContainer.classList.add('price-container');
 
-    const buttonContainer = document.createElement('div');
-    buttonContainer.classList.add('button-container');
+            const finalPriceElement = document.createElement('p');
+            finalPriceElement.style.color = 'green';
+            finalPriceElement.style.fontWeight = 'bold';
+            finalPriceElement.textContent = `${finalPrice.toFixed(2)}`;
 
-    const addToCartButton = document.createElement('button');
-    addToCartButton.classList.add('btn-primary');
-    addToCartButton.textContent = 'Add to Cart';
+            const originalPriceElement = document.createElement('del');
+            originalPriceElement.style.color = 'red';
+            originalPriceElement.textContent = `$${price.toFixed(2)}`;
 
-    const heartIcon = document.createElement('i');
-    heartIcon.classList.add('fas', 'fa-heart');
+            priceContainer.appendChild(finalPriceElement);
+            priceContainer.appendChild(document.createTextNode(' '));
+            priceContainer.appendChild(originalPriceElement);
 
-    addToCartButton.appendChild(heartIcon); // Place the heart icon inside the button
-    buttonContainer.appendChild(addToCartButton);
-    cardBody.appendChild(cardTitle);
-    cardBody.appendChild(cardText);
-    cardBody.appendChild(priceElement); 
-    card.appendChild(cardImage);
-    card.appendChild(cardBody);
-    card.appendChild(buttonContainer); // Add the button container to the card
-    productsContainer.appendChild(card);
-});
-// ...
+            const buttonContainer = document.createElement('div');
+            buttonContainer.classList.add('button-container');
 
+            const addToCartButton = document.createElement('button');
+            addToCartButton.classList.add('btn-primary');
+            addToCartButton.textContent = 'Add to Cart';
+
+            const heartIcon = document.createElement('i');
+            heartIcon.classList.add('fas', 'fa-heart');
+
+            addToCartButton.appendChild(heartIcon);
+            buttonContainer.appendChild(addToCartButton);
+            cardBody.appendChild(cardTitle);
+            cardBody.appendChild(cardText);
+            cardBody.appendChild(priceContainer);
+            cardBody.appendChild(buttonContainer);
+            card.appendChild(cardImage);
+            card.appendChild(cardBody);
+            productsContainer.appendChild(card);
+        });
     })
     .catch(error => console.error('Error:', error));
